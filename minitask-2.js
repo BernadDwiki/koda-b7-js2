437e88a43404df1ecc03a9511e3197cf7848b3f2
BernadDwiki <125329942+BernadDwiki@users.noreply.github.com>
2026-02-25 14:27:49 +0700

    Fix conditional formatting for jumBel calculation

--- minitask-2.js
+++ minitask-2.js
@@ -4,7 +4,7 @@ if(jumBel < 500000){
     jumBelFinal = jumBel;
 }else if (jumBel >= 500000 && jumBel <= 1000000){
     jumBelFinal = jumBel - (0.05 * jumBel);
-} else if (jumBel > 1000000){
+}else if (jumBel > 1000000){
     jumBelFinal = jumBel - (0.1 * jumBel);
 }
 console.log("Total belanja anda adalah "+jumBelFinal);
